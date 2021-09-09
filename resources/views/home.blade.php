731e4ec3d3460ab3160203303379cbc23b314640
Eduardo Saes <eduardodev08@gmail.com>
2021-09-09 15:15:32 -0400

    Created scaffoldin to tweets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Inicio') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,22 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <form action="">
+                        <div class="form-group row">
+                            <div class="col-md-2">
+                                <img src="{{ asset('img/user-default.jpeg') }}" alt="" class="img-thumbnail rounded float-start">
+                            </div>
+                            <div class="col-md-10">
+                                <textarea name="" id="" cols="30" rows="3" maxlength="250" class="form-control @error('username') is-invalid @enderror"></textarea>
 
-                    {{ __('You are logged in!') }}
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
