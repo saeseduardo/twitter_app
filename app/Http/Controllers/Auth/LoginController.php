e757c39cade80d71c380d826ad9602a9d6ab2784
Eduardo Saes <eduardodev08@gmail.com>
2021-09-09 14:06:26 -0400

    Modified login of users form

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,33 +8,27 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
+    protected $username;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->username = $this->findUsername();
+    }
+
+    public function findUsername(): string
+    {
+        $field = request()->input('email');
+        $fieldType = filter_var($field, FILTER_VALIDATE_EMAIL) ? 'email': 'username';
+        request()->merge([$fieldType => $field]);
+        return $fieldType;
+    }
+
+    public function username(): string
+    {
+        return $this->username;
     }
 }
